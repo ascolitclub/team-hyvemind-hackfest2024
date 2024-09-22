ebf902987bb02b3736db6df2e55fb29ec29adbdb
Deepesh Sunuwar <deepgeneral33@gmail.com>
2024-09-23 03:53:50 +0545

    about page making

--- frontend/src/components/Hero Section/HeroSection.tsx
+++ frontend/src/components/Hero Section/HeroSection.tsx
@@ -1,111 +1,3 @@
-// import { useState } from "react";
-
-// export const HeroSection = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-//   const triggerIsOpen = () => {
-//     setIsOpen(!isOpen);
-//   };
-//   return (
-//     <>
-//       <div className="h-[80vh] w-full flex items-center justify-center overflow-hidden bg-[url(/assets/7.webp)] bg-no-repeat bg-center bg-cover">
-//         <div className="Hero-section h-full container flex-col flex items-center justify-center mx-auto px-12 text-white leading-none">
-//           <p className="uppercase text-lg tracking-widest">
-//             Discover Your Next
-//           </p>
-//           <h1
-//             className="text-[150px] font-extrabold uppercase tracking-wider"
-//             style={{ fontFamily: "Oswald" }}
-//           >
-//             Perfect Stay
-//           </h1>
-//           <p className="w-[700px] text-center leading-loose">
-//             Experience comfort and convenience with our expertly curated hostel
-//             listings, designed to match students with their perfect living
-//             space.
-//           </p>
-//         </div>
-//         <div className="search absolute top-[550px] z-10 shadow-2xl box mx-72 px-8 grid items-center justify-center grid-cols-4 gap-x-12 bg-white rounded-2xl h-24 text-black font-medium">
-//           <div className="">
-//             <h2>Location</h2>
-//             <input
-//               className="outline-none py-1 rounded-md text-gray-500 font-medium"
-//               type="text"
-//               placeholder="Mitrapark, Chabahill"
-//             />
-//           </div>
-//           <div>
-//             <h2>Price Range</h2>
-//             <select
-//               className="outline-none text-gray-400"
-//               name="gender"
-//               id="gender"
-//             >
-//               <option
-//                 className="text-gray-300 outline-none"
-//                 value=""
-//                 disabled
-//                 selected
-//                 hidden
-//               >
-//                 Choose Price
-//               </option>
-//               <option>5000-10000</option>
-//               <option>10000-15000</option>
-//               <option>15000-20000</option>
-//               <option>20000-25000</option>
-//               <option>25000-Above</option>
-//             </select>
-//           </div>
-//           <div>
-//             <h2>Gender</h2>
-//             <select
-//               className="outline-none text-gray-400"
-//               name="gender"
-//               id="gender"
-//             >
-//               <option
-//                 className="text-gray-300 outline-none"
-//                 disabled
-//                 selected
-//                 hidden
-//               >
-//                 Choose Gender
-//               </option>
-//               <option>Male</option>
-//               <option>Female</option>
-//               <option>Other</option>
-//             </select>
-//           </div>
-//           <div>
-//             <button className="w-full text-2xl bg-[--btn-primary] py-4 rounded-xl font-medium text-white hover:bg-[--btn-secondary] transition-all active:translate-y-0.5">
-//               Search
-//             </button>
-//           </div>
-//         </div>
-//         <div className=" chatbot-part fixed right-0 bottom-0">
-//           <div className="absolute bg-[--primary-color] h-20 w-20 rounded-full -z-10 animate-ping"></div>
-//           <div className="chatbot-icon" onClick={triggerIsOpen}>
-//             {/* <Link to={"/"}> */}
-//             <img
-//               className=" h-20 mr-4 mb-4"
-//               src="/assets/chatbot-icon.png"
-//               alt="chatbot icon"
-//             />
-//             {/* </Link> */}
-//           </div>
-//           {isOpen && (
-//             <div
-//               data-aos={"fade-left"}
-//               data-aos-duration="100"
-//               className="chatbot-message absolute bottom-24 z-50 h-96 w-72 right-0 bg-white shadow-2xl rounded-l-lg"
-//             ></div>
-//           )}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
 import { useState } from "react";
 
 export const HeroSection = () => {
@@ -113,10 +5,12 @@ export const HeroSection = () => {
   const triggerIsOpen = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <>
-      <div className="h-[80vh] w-full flex items-center justify-center overflow-hidden bg-[url(/assets/7.webp)] bg-no-repeat bg-center bg-cover relative">
-        <div className="Hero-section h-full container flex-col flex items-center justify-center mx-auto px-12 text-white leading-none z-20">
+      <div className="h-[100vh] w-full flex items-center flex-col justify-center overflow-hidden bg-[url(/assets/7.webp)] bg-no-repeat bg-center bg-cover relative">
+        <div className="bg-black/35 absolute h-full w-full"></div>
+        <div className="Hero-section mt-12 h-full container flex-col flex items-center justify-center mx-auto px-12 text-white leading-none z-10">
           <p className="uppercase text-lg tracking-widest">
             Discover Your Next
           </p>
@@ -126,74 +20,77 @@ export const HeroSection = () => {
           >
             Perfect Stay
           </h1>
-          <p className="w-[700px] text-center leading-loose">
+          <p className="w-[700px] text-center leading-loose pb-16">
             Experience comfort and convenience with our expertly curated hostel
             listings, designed to match students with their perfect living
             space.
           </p>
-        </div>
-        <div className="search absolute top-[400px] z-10 shadow-2xl box mx-72 px-8 grid items-center justify-center grid-cols-4 gap-x-12 bg-white rounded-2xl h-24 text-black font-medium">
-          <div className="">
-            <h2>Location</h2>
-            <input
-              className="outline-none py-1 rounded-md text-gray-500 font-medium"
-              type="text"
-              placeholder="Mitrapark, Chabahill"
-            />
-          </div>
-          <div>
-            <h2>Price Range</h2>
-            <select
-              className="outline-none text-gray-400"
-              name="gender"
-              id="gender"
-            >
-              <option
-                className="text-gray-300 outline-none"
-                value=""
-                disabled
-                selected
-                hidden
+          <div className="search shadow-2xl z-20 w-[70%] box mx-72 px-8 grid items-center justify-center grid-cols-4 gap-x-12 bg-white rounded-2xl h-24 text-black font-medium">
+            <div className="">
+              <h2>Location</h2>
+              <input
+                className="outline-none py-1 rounded-md text-gray-500 font-medium"
+                type="text"
+                placeholder="Mitrapark, Chabahill"
+              />
+            </div>
+            <div>
+              <h2>Price Range</h2>
+              <select
+                className="outline-none text-gray-400"
+                name="gender"
+                id="gender"
               >
-                Choose Price
-              </option>
-              <option>5000-10000</option>
-              <option>10000-15000</option>
-              <option>15000-20000</option>
-              <option>20000-25000</option>
-              <option>25000-Above</option>
-            </select>
-          </div>
-          <div>
-            <h2>Gender</h2>
-            <select
-              className="outline-none text-gray-400"
-              name="gender"
-              id="gender"
-            >
-              <option
-                className="text-gray-300 outline-none"
-                disabled
-                selected
-                hidden
+                <option
+                  className="text-gray-300 outline-none"
+                  value=""
+                  disabled
+                  selected
+                  hidden
+                >
+                  Choose Price
+                </option>
+                <option>5000-10000</option>
+                <option>10000-15000</option>
+                <option>15000-20000</option>
+                <option>20000-25000</option>
+                <option>25000-Above</option>
+              </select>
+            </div>
+            <div>
+              <h2>Gender</h2>
+              <select
+                className="outline-none text-gray-400"
+                name="gender"
+                id="gender"
               >
-                Choose Gender
-              </option>
-              <option>Male</option>
-              <option>Female</option>
-              <option>Other</option>
-            </select>
-          </div>
-          <div>
-            <button className="w-full text-2xl bg-[--btn-primary] py-4 rounded-xl font-medium text-white hover:bg-[--btn-secondary] transition-all active:translate-y-0.5">
-              Search
-            </button>
+                <option
+                  className="text-gray-300 outline-none"
+                  disabled
+                  selected
+                  hidden
+                >
+                  Choose Gender
+                </option>
+                <option>Male</option>
+                <option>Female</option>
+                <option>Other</option>
+              </select>
+            </div>
+            <div>
+              <button className="w-full text-2xl bg-[--btn-primary] py-4 rounded-xl font-medium text-white hover:bg-[--btn-secondary] transition-all active:translate-y-0.5">
+                Search
+              </button>
+            </div>
           </div>
         </div>
         {/* Chatbot Part */}
-        <div className="chatbot-part fixed right-0 bottom-0 z-50">
+        <div className="chatbot-part fixed right-0 bottom-0 z-30">
           <div className="absolute bg-[--primary-color] h-20 w-20 rounded-full -z-10 animate-ping"></div>
-          <div className="chatbot-icon cursor-pointer" onClick={triggerIsOpen}>
+          <div
+            className="chatbot-icon cursor-pointer z-30"
+            onClick={triggerIsOpen}
+          >
             <img
               className="h-20 mr-4 mb-4"
               src="/assets/chatbot-icon.png"
@@ -204,7 +101,7 @@ export const HeroSection = () => {
             <div
               data-aos={"fade-left"}
               data-aos-duration="100"
-              className="chatbot-message absolute bottom-24 z-50 h-96 w-max right-0 bg-white shadow-2xl rounded-l-lg"
+              className="chatbot-message absolute bottom-24 z-40 h-96 w-max right-0 bg-white shadow-2xl rounded-l-lg"
             >
               <iframe
                 style={{ borderRadius: "7px 0" }}
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,7 +1,63 @@
+import { Banner } from "../components/features/Banner";
+import { HowItWorks } from "../components/features/HowItWorks";
+import OurFeatures from "../components/features/OurFeatures";
+import { QualityAssuarance } from "../components/features/QualityAssuarance";
+import PopularHostel from "../components/popularhostel/PopularHostel";
+
 export default function About() {
   return (
-    <div className="text-6xl text-red-700 font-bold text-center">
-      WELCOME TO ABOUTPAGE BUDDY
+    <div className="bg-[url(/assets/2.webp)] bg-center bg-no-repeat bg-cover absolute top-0 w-full h-[100vh] ">
+      <div className=" h-full">
+        <div className="Hero-section mt-12 h-full container flex-col flex items-center justify-center mx-auto px-12 text-white leading-none">
+          <h1
+            className="text-[150px] font-extrabold uppercase tracking-wider"
+            style={{ fontFamily: "Oswald" }}
+          >
+            About us
+          </h1>
+          <p className="w-[700px] text-center leading-loose mt-8">
+            Experience comfort and convenience with our expertly curated hostel
+            listings, designed to match students with their perfect living
+            space.
+          </p>
+          <div className="images grid grid-cols-4 items-center gap-4 h-52 mt-8">
+            <div className="h-52 w-auto shadow-2xl">
+              <img
+                className="rounded-2xl h-full"
+                src="/assets/3.webp"
+                alt="images"
+              />
+            </div>
+            <div>
+              <img
+                className="rounded-2xl shadow-2xl "
+                src="/assets/4.webp"
+                alt="images"
+              />
+            </div>
+            <div className="h-52 w-auto shadow-2xl">
+              <img
+                className="rounded-2xl h-full"
+                src="/assets/5.webp"
+                alt="images"
+              />
+            </div>
+            <div>
+              <img
+                className="rounded-2xl shadow-2xl"
+                src="/assets/10.webp"
+                alt="images"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <OurFeatures />
+      <PopularHostel />
+      <HowItWorks />
+      <QualityAssuarance />
+      <Banner />
     </div>
   );
 }
