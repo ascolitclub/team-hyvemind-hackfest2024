00dd0d467711dbaf370967c77a4814201ac8cadd
SubhamJoshi18 <shubhamrajjoshi69@gmail.com>
2024-09-22 21:00:58 +0545

    feat: add folder structure for model-microservice

--- model-microservice/index.py
+++ model-microservice/index.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,jsonify
+from map.helper.map import get_hostel
 
 app = Flask(__name__)
 
@@ -13,7 +14,8 @@ app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-
 
 @app.get('/model')
 def run():
-    return "{\"message\" : \"Hey there python\"}"
+    result = get_hostel()
+    return jsonify(result), 200
 
 
 if __name__ == "__main__":
