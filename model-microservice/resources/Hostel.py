7692fa21b4cc490a09971a5a2472d20fab03f6c3
SubhamJoshi18 <shubhamrajjoshi69@gmail.com>
2024-09-23 08:48:01 +0545

    json config

--- model-microservice/resources/Hostel.py
+++ model-microservice/resources/Hostel.py
@@ -1,10 +1,11 @@
 from flask_smorest import Blueprint
 from flask.views import MethodView
 from flask import jsonify
+from bson import ObjectId
 from libs.logger import logger
 from models.trainModel.model1 import recommend_hostels
 from extractor.extractmodel import recommend_hotel_api
-
+from services.hostelService import getHostelById
 
 blp = Blueprint('hostel', __name__, description='Hostel Routes')
 
@@ -25,3 +26,20 @@ class PopularHostel(MethodView):
             return jsonify({"error": "An error occurred while fetching popular hostels."}), 500
 
 
+@blp.route('/hostel/<string:hostelId>')
+class Hostel(MethodView):
+    def get(self, hostelId):
+        try:
+            # Fetch the hostel by ID
+            response = getHostelById(hostelId=ObjectId(hostelId)) 
+            logger.info('Fetched hostel response: %s', response)
+
+            if response and '_id' in response:
+                response['_id'] = str(response['_id'])  # Convert ObjectId to string
+            
+            # Structure the response
+            return jsonify({'data': response}), 200
+
+        except Exception as e:
+            logger.error(f'Error fetching particular hostel: {e}')
+            return jsonify({'error': 'An error occurred while fetching the hostel ID'}), 500
